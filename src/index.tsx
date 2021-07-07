76c46ca9737282a210d1588e4b2f492dd848bc61
allyroo <allyse.arucan@gmail.com>
2021-07-06 22:54:15 -0700

    day 1 commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Home, Dashboard, SignIn } from "./components"; //NEW_ADDITION
+import reportWebVitals from "./reportWebVitals";
+import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home title={"Real Fake Cars"} />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard></Dashboard>
+        </Route>
+
+        <Route path="/signin">
+          <SignIn></SignIn>
+        </Route>
+      </Switch>
+    </Router>
+    {/* NEW_ADDITION*/}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
